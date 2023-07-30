6e1e254e3045b26d26cdfc744d2b74305e985d91
ak-202 <aamankumarr111@gmail.com>
2023-07-30 11:11:25 +0530

    update

---
+++ src/Recipe.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './recipe.module.css';
+
+
+const Recipe= ({title,calories,image,ingredients})=>{
+    return (
+      <div>
+        <h1 className="{style.recipe}">{title}</h1>
+        <ol>
+          {ingredients.map((ingredient) => (
+            <li>{ingredient.text}</li>
+          ))}
+        </ol>
+        <p>{calories}</p>
+        <img src={image} alt="" />
+      </div>
+    );
+}
+
+export default Recipe;
